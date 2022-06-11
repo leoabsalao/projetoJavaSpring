6d17551c8799a3c82616d2dbf1ed431994e290a9
Leo Absalao <leonardoabsalao@hotmail.com>
2022-06-10 23:38:02 -0300

    Proejto iniciado

---
+++ src/main/java/com/leonardoabsalao/vendas/MinhaConfig.java
@@ -0,0 +1,14 @@
+package com.leonardoabsalao.vendas;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MinhaConfig {
+	
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
